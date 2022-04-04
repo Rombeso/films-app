7b9f59f27e91d757554732d916bbf9c78447b585
Rombeso <larionovra@gmail.com>
2022-04-04 16:24:49 +0300

    Added new header

---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import { AppPropsType } from 'next/dist/shared/lib/utils'
+import '../styles/globals.css'
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }: AppPropsType): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>My films- app</title>
+        <link key={1} rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
