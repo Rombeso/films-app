74e876d77ee5a13b02c88dc66b3cb64d0967c63b
Rombeso <larionovra@gmail.com>
2022-04-10 21:28:23 +0300

    Added Layout and HOK withLayout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
 import { Button, Htag, Paragraph, Rating, Tag } from '../components'
+import { Layout, withLayout } from '../layout/Layout'
 
-
-
-export default function Home(): JSX.Element {
+function Home(): JSX.Element {
 
   const [rating, setRating] = useState<number>(4)
 
@@ -22,3 +21,5 @@ export default function Home(): JSX.Element {
     </>
   )
 }
+
+export default withLayout(Home)
