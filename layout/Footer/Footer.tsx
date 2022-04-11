0064102f55d421740d873c2bbd68311daa44bead
Rombeso <larionovra@gmail.com>
2022-04-11 14:51:30 +0300

    Added grid

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,11 +1,14 @@
 import styles from './Footer.module.css'
 import { FooterProps } from './Footer.props'
 import cn from 'classnames'
+import { format } from 'date-fns'
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer {...props} className={cn(className, styles.footer)}>
+			<span className={styles.rights}>OwlTop © 2021 - {format(new Date, 'yyyy')} Все права защищены</span>
+			<a href='#' target="_blank" className={styles.agreement}>Пользовательское соглашение</a>
+			<a href='#' target="_blank" className={styles.privacy}>Политика конфиденциальности</a>
+		</footer>
 	)
 }
